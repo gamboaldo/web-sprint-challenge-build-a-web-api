34e121f984b3cf03f5be178188cd87ff5d5577e0
Aldo Mata <gamboaldo@gmail.com>
2021-07-09 10:20:56 -0700

    one more bug

--- api/projects/projects-middleware.js
+++ api/projects/projects-middleware.js
@@ -1,1 +1,22 @@
 // add middlewares here related to projects
+const Projects = require("../projects/projects-model");
+
+async function validateProjectId(req, res, next) {
+  try {
+    const project = await Projects.get(req.params.id);
+    if (!project) {
+      res.status(404).json({
+        message: "no project exists",
+      });
+    } else {
+      req.project = project;
+      next();
+    }
+  } catch (err) {
+    res.status(500).json({
+      message: " cant find project id",
+    });
+  }
+}
+
+module.exports = { validateProjectId };
--- api/projects/projects-router.js
+++ api/projects/projects-router.js
@@ -1,1 +1,86 @@
 // Write your "projects" router here!
+const express = require("express");
+//
+const { validateProjectId } = require("./projects-middleware");
+//
+const router = express.Router();
+const Projects = require("./projects-model");
+
+router.get("/", (req, res, next) => {
+  Projects.get()
+    .then((projects) => res.json(projects))
+    .catch(next);
+});
+
+router.get("/:id", validateProjectId, (req, res) => {
+  res.json(req.project);
+});
+
+router.post("/", async (req, res, next) => {
+  const { name, description } = req.body;
+  try {
+    if (!name || !description) {
+      res.status(400).json({ message: "needs both name and description" });
+    } else {
+      await Projects.insert(req.body).then((newPro) =>
+        res.status(201).json(newPro)
+      );
+    }
+  } catch (err) {
+    next(err);
+  }
+});
+
+router.put("/:id", (req, res, next) => {
+  const { name, description } = req.body;
+  if (!name || !description) {
+    res.status(400).json({ message: "u need body info" });
+  } else {
+    Projects.update(req.params.id, req.body)
+      .then(() => {
+        return Projects.get(req.params.id);
+      })
+      .then((newPro) => {
+        res.json(newPro);
+      })
+      .catch(next);
+  }
+});
+
+router.delete("/:id", validateProjectId, async (req, res, next) => {
+  const { id } = req.params;
+  try {
+    const proToDelete = await Projects.get(id);
+    const deletedPro = await Projects.remove(id);
+    deletedPro
+      ? res.json(proToDelete)
+      : res.status(500).json({ message: "messed up delete" });
+  } catch (err) {
+    next(err);
+  }
+});
+
+router.get("/:id/actions", validateProjectId, async (req, res, next) => {
+  // const { id } = req.params.id;
+  // Projects.getProjectActions(id)
+  //   .then((projects) => {
+  //     res.json(projects);
+  //   })
+  //   .catch(next);
+  try {
+    const actions = await Projects.getProjectActions(req.params.id);
+    res.json(actions);
+  } catch (err) {
+    next(err);
+  }
+});
+
+router.use((err, req, res) => {
+  res.status(err.status || 500).json({
+    customMessage: "something broke in projects router",
+    message: err.message,
+    stack: err.stack,
+  });
+});
+
+module.exports = router;
